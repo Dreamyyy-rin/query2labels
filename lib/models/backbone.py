b427619d386da57a9b53a3a1c1ca76f4bdd8e0f5
Oivicko Ekagani Irwanto <172259456+Dreamyyy-rin@users.noreply.github.com>
2025-10-14 21:29:45 +0700

    Update backbone.py

--- lib/models/backbone.py
+++ lib/models/backbone.py
@@ -16,7 +16,7 @@ from typing import Dict, List
 
 import models
 from lib.models.cls_cvt import build_CvT
-from models.swin_transformer import build_swin_transformer
+from lib.models.swin_transformer import build_swin_transformer
 
 from utils.misc import clean_state_dict
 
